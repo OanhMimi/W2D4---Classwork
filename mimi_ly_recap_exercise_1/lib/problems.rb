4828c525e7d74ced5825e3fb565ffa21d4f2caca
Mimi <lyoanhmymimi@gmail.com>
2022-06-24 00:11:54 -0700

    W2D3 ClassWork

--- mimi_ly_recap_exercise_1/lib/problems.rb
+++ mimi_ly_recap_exercise_1/lib/problems.rb
@@ -6,24 +6,32 @@
 #
 # all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
 def all_vowel_pairs(words)
-
     new_arr = []
     words.each_with_index do |word1,idx1|
         words.each_with_index do |word2,idx2|
-            if idx2>idx2
-                new_arr = [word1, word2]
+            if idx2>idx1 && findVowels(word1,word2)
+                new_arr << word1 + " " + word2
             end 
         end
     end
-    new_arr.each do |subArray|
+    new_arr
+end
 
+def findVowels(w1,w2)
+    hash = {"a" => 0, "e" => 0, "i" => 0, "o" => 0, "u" => 0}
+    w1.each_char do |char|
+        if hash.has_key?(char)
+            hash[char] += 1
+        end
     end
-
+    w2.each_char do |char|
+        if hash.has_key?(char)
+            hash[char] += 1
+        end
+    end
+    hash.each_value {|v|return false if v < 1}
+    true
 end
-
-def 
-end
-
 
 # Write a method, composite?, that takes in a number and returns a boolean indicating if the number
 # has factors besides 1 and itself
@@ -33,9 +41,9 @@ end
 # composite?(9)     # => true
 # composite?(13)    # => false
 def composite?(num)
-
+    (2...num).each{|factor| return true if num % factor == 0}
+    false
 end
-
 
 # A bigram is a string containing two letters.
 # Write a method, find_bigrams, that takes in a string and an array of bigrams.
@@ -47,7 +55,7 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-
+    bigrams.select{|bigram| str.include?(bigram)}
 end
 
 class Hash
@@ -65,7 +73,8 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
-
+        prc||= Proc.new{|k,v| k == v}
+        self.select{|k,v| prc.call(k,v)}
     end
 end
 
@@ -79,7 +88,17 @@ class String
     # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
     # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
-
+        new_arr = []
+        (0...self.length).each do |i|
+            (i...self.length).each do |i2|
+                new_arr <<self[i..i2]
+            end
+        end
+        if length.nil?
+            return new_arr
+        else
+            new_arr.select {|ele| ele.length == length}
+        end
     end
 
 
@@ -93,6 +112,14 @@ class String
     # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
     # "zebra".caesar_cipher(4)    #=> "difve"
     def caesar_cipher(num)
-
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        new_string = ""
+        self.each_char do |char|
+            old_index = alphabet.index(char)
+            new_index = old_index + num
+            new_string += alphabet[new_index % alphabet.length]
+        end
+        new_string
     end
+
 end
